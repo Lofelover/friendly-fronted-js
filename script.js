f688c8b6b558829218ce1867c99339bed226cb4c
Lofelover <markovilia007@gmail.com>
2026-07-27 02:21:16 +0300

    variables

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-console.log('Hello, word!')
+'use strict'
 
+const name = 'Ilya'
+const greeting = 'Hello'
 
+const message = `${name}, ${greeting}!`
 
-console.log('Привет')
+console.log(message)
 
-//Hi!
+const a = 400000
+const b = 4000
+
+const sum = `Сумма равна = ${a + b}`
+
+console.log(sum)
+
+const a = 1_000 + 1_0
+
+console.log(a)
+
+console.log(
+  typeof a
+)
+
+const b = 100
+
+console.log(Boolean(b))
+console.log(typeof String(b))
