7ff1f6551fa6f53597a2d99136bb9bae9dfdeed6
Lofelover <markovilia007@gmail.com>
2026-08-03 03:01:47 +0300

    object

--- script.js
+++ script.js
@@ -1,59 +1,148 @@
 'use strict'
 
-// function logMessage(message, count) {
-//   for (let i = 0; i < count; i++) {
-//     console.log(message)
-//   }
+// 1.1
+// const user = {
+//     name: 'Алексей',
+//     age: 30,
+//     isAdmin: false,
 // }
 
-// logMessage('Хейййй', 3)
+// console.log(user.name, user.age, user.isAdmin)
+// console.log(user['name'], user['age'], user['isAdmin'])
 
+// 1.2 
 
-// function multiply(a, b) {
-//     return a * b
+// user.city = 'Москва'
+
+// console.log(user)
+
+// user.email = 'alex@example.com'
+
+// console.log(user)
+
+// user.age = 31
+
+// console.log(user)
+
+// user.isAdmin = true
+
+// console.log(user)
+
+// 1.3
+
+// delete user.email
+
+// console.log(user)
+
+// delete user.city
+
+// console.log(user)
+
+// // 2.1 
+
+// const name = "Мария";
+// const age = 25;
+// const job = "Дизайнер";
+
+// const person = {
+//     name,
+//     age,
+//     job,
 // }
 
-// const multiplyExpr = function(a, b) {
-//   return a * b
+// console.log(person)
+
+
+// // 2.2
+
+// const title = "Ноутбук";
+// const price = 1500;
+// const inStock = true;
+
+// const product = {
+//     title,
+//     price,
+//     inStock,
+//     category: 'Категория',
+//     discount: 10,
 // }
 
-// const multiplyArrow = (a, b) => {
-//   return a * b;
+// console.log(product)
+
+// // 3.1
+
+// const config = {
+//     ['user name']: 'Иван',
+//     ['user-age']: 28,
+//     ['is_admin']: true,
 // }
 
-// const isEven = (number) => !Boolean(number % 2)
+// console.log(config)
 
-// console.log(isEven(4))
+// // 3.2
 
-// 3.1 
+// const key = "status";
+// const value = "active";
 
-// function greet(name) {
-//   return `Привет, ${name}`
+// const Object = {
+//     [key]: value,
 // }
 
-// const sayHi = greet
+// console.log(Object)
 
-// console.log(sayHi('Илья'))
+// // 4.1 
 
-// 3.2
+// const book = {
+//   title: "Война и мир",
+//   author: "Толстой",
+//   pages: 1200
+// };
 
-// function processUserInput(callback) {
-//   const sign = prompt('Как тебя зовут?')
-//   callback(sign)
+// console.log('title' in book)
+// console.log('year' in book)
+// console.log('author' in book)
+// console.log('pages' in book)
+
+// // 4.2
+
+// console.log(book.publisher)
+// console.log('publisher' in book)
+
+// // 5.1 
+
+// const country = {
+//   name: "Россия",
+//   capital: "Москва",
+//   population: 146000000,
+//   language: "Русский"
+// };
+
+// for (const option in country) {
+//     console.log(country[option])
 // }
 
+// // 5.2 
 
-// function yourName(name) {
-//   console.log(`Тебя зовут ${name}`)
+// for (const option in country) {
+//     console.log(`${option}: ${country[option]}`)
 // }
 
-// processUserInput(yourName)
+// 5.3
 
-function createMultiplier(multiplier) {
-    return function(n) {
-        return n * multiplier;
-    };
+const student = {
+  name: "Ольга",
+  grades: {
+    math: 5,
+    history: 4,
+    english: 5
+  }
+};
+
+for (const option in student) {
+    if (typeof student[option] === 'object') {
+        const gradesObject = student[option];
+        for (const key in gradesObject) {
+            console.log(`Оценка по ${key}: ${gradesObject[key]}`)
+        }
+    }
 }
-
-const double = createMultiplier(2);
-console.log(double); // Должно вывести 10
