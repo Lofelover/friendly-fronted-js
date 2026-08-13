a1beae6e6e481356d45317f788ac35ce50d55b30
Lofelover <markovilia007@gmail.com>
2026-08-14 01:01:40 +0300

    свойства и методы строк

--- script.js
+++ script.js
@@ -1,17 +1,86 @@
 'use strict'
 
+// const str = "JavaScript"
 
-const chainCalculator = {
-    value: 0,
-    add(n) {
-        this.value += n
-        return this
-    },
-    substract(n) {
-        this.value -= n
-        return this
-    },
+// console.log(str.length)
+// console.log(str[0])
+// console.log(str[ str.length - 1])
+// console.log(str.at(-1))
+// console.log(str.at(10))
+// console.log(str[10])
+
+
+// const input = " приВЕТ МИР! ";
+
+// console.log(input.trim())
+// console.log(input.trim().toLowerCase())
+// console.log(input.trim().toUpperCase())
+// console.log(input.trimStart().trimEnd())
+
+// const text = "Я люблю JavaScript, JavaScript — это круто!"
+
+// console.log(text.indexOf('JavaScript', text.indexOf('JavaScript') + 1))
+// console.log(text.includes('круто'))
+// console.log(text.startsWith('Я'))
+// console.log(text.endsWith('!'))
+
+// const phrase = "apple, banana, apple, orange"
+
+// const indexApple = phrase.indexOf('apple')
+
+// console.log(phrase.indexOf('apple', indexApple + 1))
+// console.log(phrase.includes('banana', 10))
+
+// const str = "Hello, World!";
+
+// console.log(str.substring(0, 5))
+// console.log(str.slice(0, 5))
+
+// console.log(str.substring(7))
+// console.log(str.slice(7))
+
+// console.log(str.substring(-6))
+// console.log(str.slice(-6))
+
+// const input = 'ha'
+
+// console.log(input.repeat(3))
+
+// const msg = "Кот — домашнее животное. Кот любит спать."
+
+// console.log(msg.replace('Кот', 'Пёс'))
+// console.log(msg.replaceAll('Кот', 'Пёс'))
+
+// const csv = "Иван,Петр,Мария,Ольга"
+
+// console.log(csv.split(','))
+// console.log(csv.split(' '))
+// console.log(csv.split(''))
+
+// console.log(csv.split(',').join(';'))
+
+// let s = "hello"
+
+// console.log(s.slice(0, 1) + 'x' + s.slice(2))
+
+function ValidateEmail(email) {
+  const clearEmail = email.trim()
+
+  if (!clearEmail) {
+    return 'Ошибка'
+  }
+
+  if (!clearEmail.includes('@'))
+    return 'Ошибка'
+
+  if (clearEmail.startsWith('@'))
+    return 'Ошибка'
+
+  if (clearEmail.endsWith('.com') || clearEmail.endsWith('.ru')) {
+    return 'Ошибка'
+  }
+
+  return true
 }
 
-chainCalculator.add(5).substract(2)
-console.log(chainCalculator.value)
+ValidateEmail('markovilia007@gmail.com')
