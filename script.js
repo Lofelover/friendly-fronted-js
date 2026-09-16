7d1d5464370de5c7a01b97ad441d359a0731789d
Lofelover <markovilia007@gmail.com>
2026-09-16 18:50:01 +0300

    промисы + ивент луп

--- script.js
+++ script.js
@@ -1,99 +1,83 @@
 'use strict'
 
-// 1.1
-// try {
-//   console.log(unknownVar)
-// }
-// catch(error) {
-//   console.log(`Произошла ошибка: ${error}`)
+// // 1.2
+
+// console.log("Начало");
+
+// // Запоминаем время старта цикла в миллисекундах
+// const startTime = Date.now();
+
+
+// while (Date.now() - startTime < 2000) {
+
 // }
 
-// 1.2
+// console.log("Конец");
 
-// function divide(a, b) {
-//   if (b === 0) {
-//     throw new Error('Деление на ноль')
-//   } 
-//   return a / b
-// }
+// setTimeout(() => {
+//   console.log("Я должен был выполниться раньше")
+// }, 0)
 
-// try {
-//   divide(10, 0)
-// } catch(error) {
-//   console.log(`Возникла ошибка ${error}`)
-// }
 
 // 2.1
 
-// try {
-//   eval("console.log('Привет'); // незакрытая строка");
-// } catch (error) {
-//   console.log("Ошибка поймана");
+// function greetUser(name, callback) {
+//   console.log(`Привет, ${name}`)
+//   callback();
 // }
 
-// я не вижу здесь ошибок, так-то строка закрыта в консоль логе все корректно
+// greetUser('Илья', () => {
+//   console.log('Колбэк выполнен')
+// })
 
-// 3.1 и 3.2
+// 2.2
 
-// setTimeout(() => {
-//   try {
-//     throw new Error("Ошибка в таймере");
-//   } catch (error) {
-//     console.log("Ошибка поймана:", error.message);
-//   }
-// }, 1000);
-
-// Ошибка ловится только в том случае, если констукций трай кетч находится внутри асинхронного кода. Здесь я перенес конструкцию внутрь и все заработало
-
-// 4.1
-
-// try {
-//   JSON.parse('{ некорректный JSON }')
-// } catch(error) {
-//   console.log('error.name', error.name)
-//   console.log('error.message', error.message)
-//   console.log('error.stack',error.stack)
+// function loadData(callback) {
+//   setTimeout(() => {
+//     console.log('Данные загружены')
+//   }, 1000)
+//   callback()
 // }
 
-// 4.2
+// loadData(() => {
+//   console.log('Обработка данных завершена')
+// })
 
-// function parseJSON(str) {
-//   try {
-//     return JSON.parse(str)
-//   } catch (error) {
-//     console.log(error.name)
-//     console.log(error.message)
-//     return null
-//   }
+// function getUser(callback) {
+//   setTimeout(() => {
+//     callback({ id: 1, name: "Анна" })
+//   }, 1000)
+  
 // }
 
-// // console.log(parseJSON('{"name":"Анна"}'))
-// console.log(parseJSON('{name: "Анна"}'))
-
-// 5.1
-
-// function checkAge(age) {
-//   if (age < 0) {
-//     throw new Error('Возраст не может быть отрицательным')
-//   }
-//   else if (age > 120) {
-//     throw new Error('Возраст не может быть больше 120')
-//   }
-//   return 'Возраст корректен'
+// function getOrders(userId, callback) {
+//   setTimeout(() => {
+//     callback(["Заказ 1", "Заказ 2"])
+//   }, 1000)
 // }
 
-// try {
-//   checkAge(130)
-// } catch (error) {
-//   console.log(error)
+// function getOrderDetails(orderId, callback) {
+//   setTimeout(() => {
+//     callback(`Детали заказа: ${orderId}`)
+//   }, 1000)
 // }
 
-// 6.1
+// getUser((user) => {
+//     console.log("Пользователь:", user);
+//     getOrders(user.id, (orders) => {
+//         console.log("Заказы:", orders);
+//         getOrderDetails(orders[0], (details) => {
+//             console.log(details);
+//         });
+//     });
+// });
 
-try {
-  console.log(asdasdasd)
-} catch(error) {
-  console.log(error)
-} finally {
-  console.log('Закрываем файл')
-}
+// 3.1
+
+const promise = new Promise((fullfill, reject) => {
+  setTimeout(() => {
+    fullfill('Успех')
+  }, 1000)
+})
+
+promise.then(result => console.log(result))
