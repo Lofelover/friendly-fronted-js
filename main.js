fd8a10ffea0eb8721a1a72c6b29957b548a0119b
Lofelover <markovilia007@gmail.com>
2026-09-17 19:05:45 +0300

    декомпозиция кода

---
+++ main.js
@@ -0,0 +1,23 @@
+// import { add, substract, multiply } from "./math.js";
+
+// console.log(add(2, 3))
+// console.log(substract(2, 3))
+// console.log(multiply(2, 3))
+
+// import { div } from "./math.js"
+
+// console.log(div(6, 3))
+
+// import { multiply as mul } from "./math.js"
+
+// console.log(mul(6, 3))
+
+// import Greeting from "./greet.js"
+
+// console.log(Greeting('Илья'))
+
+import * as utils from "./index.js"
+
+utils.add(2, 3)
+utils.multiply(2, 3)
+utils.substract(2, 3)
---
+++ math.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  console.log(a + b)
+}
+
+function substract(a, b) {
+  console.log(a - b) 
+}
+
+function multiply(a, b) {
+  console.log(a * b) 
+}
+
+export { add, substract, multiply }
+
